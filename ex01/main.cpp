a4ae91db9f83cb2ebb1aaa6bac3118d707d27b3c
root <root@LAPTOP-BAKKV618.localdomain>
2022-08-03 20:49:13 +0200

    ex01

---
+++ ex01/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+struct Data{
+	int	a;
+	float	b;
+	char	c;
+};
+
+uintptr_t serialize(Data* ptr){
+	return reinterpret_cast<uintptr_t>(ptr);}
+
+Data* deserialize(uintptr_t raw){
+	return reinterpret_cast<Data *>(raw);}
+
+int main()
+{
+	Data data;
+
+	std::cout << &data << std::endl;
+	std::cout << serialize(&data) << std::endl;
+	std::cout << deserialize(serialize(&data)) << std::endl;
+	return 0;
+}
